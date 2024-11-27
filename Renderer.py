050b6a88ab374968399c92a93a54d998d63f5862
Ahmed mohamed taha <ahmed.wesal03@eng-st.cu.edu.eg>
2024-11-27 11:03:27 +0200

    hotfix: reserving instances and integrating with tumorfinder

--- Renderer.py
+++ Renderer.py
@@ -10,20 +10,22 @@ from TumorFinder import TumorFinder
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.current_layout = None
-        self.setupUi("Analysis")
-        self.TumorFinder = TumorFinder()
-        
-             
+        self.current_layout = None  
+        decompress = Decompression()
+        compress = Compression()
+        analysis = Analysis()
+        self.arr = [decompress, compress, analysis]
+        self.setupUi("Analysis")   
+        self.tumor_finder = None      
         
 
     def setupUi(self, layout_name):
         if layout_name == "Decompression":
-            self.current_layout = Decompression()
+            self.current_layout = self.arr[0]
         elif layout_name == "Compression":
-            self.current_layout = Compression()
+            self.current_layout = self.arr[1]
         elif layout_name == "Analysis":
-            self.current_layout = Analysis()
+            self.current_layout = self.arr[2]
 
         self.current_layout.setupUi(self)
         if layout_name == "Decompression":
@@ -35,6 +37,9 @@ class MainWindow(QMainWindow):
         elif layout_name == "Analysis":
             self.current_layout.compress_button.clicked.connect(lambda: self.switch_layout("Compression"))
             self.current_layout.decompress_button.clicked.connect(lambda: self.switch_layout("Decompression"))
+            self.tumor_finder = TumorFinder(self.current_layout)
+            self.tumor_finder.load_image()
+            
 
     def switch_layout(self, layout_name):
         self.current_layout = layout_name
